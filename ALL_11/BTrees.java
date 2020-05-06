d45ba84e85581e73a0ce5b40f0f5fdaa513366b4
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-05-06 19:13:59 +0200

    Uncommented line

--- ALL_11/BTrees.java
+++ ALL_11/BTrees.java
@@ -5,7 +5,7 @@ public class BTrees {
 
   public static void main(String[] args) {
     BTree tree = new BTree(3);
-    // tree.add(List.of(62, 2, 88, 6, 83, 81, 49, 71, 2, 69, 74, 90, 23));
+    tree.add(List.of(62, 2, 88, 6, 83, 81, 49, 71, 2, 69, 74, 90, 23));
     // tree.add(List.of(5, 55, 555, 5555, 55555));
     // tree.add(List.of(1, 2, 3, 4, 5));
     // tree.print();
